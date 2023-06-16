f57282fffafb663236d320851b7d4bcc59686246
Ekoh Franklin <ekohfranklin@gmail.com>
2023-06-16 06:35:28 +0100

    added pagintor service class

--- src/Service/PaginatorService.php
+++ src/Service/PaginatorService.php
@@ -2,7 +2,53 @@
 
 namespace App\Service;
 
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\ORM\Tools\Pagination\Paginator as OrmPaginator;
+
 class PaginatorService
 {
+    private int $total;
+    private int $lastPage;
+    private Paginator $items;
 
+    /**
+     * @param QueryBuilder|Query $query
+     * @param int $page
+     * @param int $limit
+     * @return PaginatorService
+     */
+    public function paginate($query, int $page = 1, int $limit = 10): PaginatorService
+    {
+        $paginator = new OrmPaginator($query);
+
+        $paginator
+            ->getQuery()
+            ->setFirstResult($limit * ($page - 1))
+            ->setMaxResults($limit);
+
+        $this->total = $paginator->count();
+        $this->lastPage = (int) ceil($paginator->count() / $paginator->getQuery()->getMaxResults());
+        $this->items = $paginator;
+
+        return $this;
+    }
+
+    public function getTotal(): int
+    {
+        return $this->total;
+    }
+
+    public function getLastPage(): int
+    {
+        return $this->lastPage;
+    }
+
+    public function getItems()
+    {
+        return $this->items;
+    }
 }
