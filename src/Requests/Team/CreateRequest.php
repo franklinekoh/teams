7f19fd73a957500b0f8a57479298a693f532adb3
Ekoh Franklin <ekohfranklin@gmail.com>
2023-06-16 06:36:54 +0100

    Team service and request classes

--- src/Requests/Team/CreateRequest.php
+++ src/Requests/Team/CreateRequest.php
@@ -2,7 +2,38 @@
 
 namespace App\Requests\Team;
 
-class CreateRequest
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Type;
+use App\Requests\BaseRequest;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class CreateRequest extends BaseRequest
 {
+    #[Type('string')]
+    #[NotBlank([])]
+    #[Assert\Length(['min' => 3])]
+    protected $name;
+
+    #[Type('string')]
+    #[NotBlank([])]
+    #[Assert\Length(['min' => 3])]
+    protected $country;
+
+    #[Type('numeric')]
+    #[NotBlank([])]
+    protected $money_balance;
+
+    #[NotBlank([])]
+    #[Type('array')]
+    #[Assert\All([
+        new Assert\Collection([
+            'fields' => [
+                'first_name' => [new Assert\NotBlank(), new Assert\Type(['type' => 'string'])],
+                'last_name' => [new Assert\NotBlank(), new Assert\Type(['type' => 'string'])]
+            ]
+        ])
+    ])]
+    protected $players;
+
 
 }
--- src/Service/TeamService.php
+++ src/Service/TeamService.php
@@ -1,8 +1,50 @@
 <?php
 
-namespace App\Services;
+namespace App\Service;
+
+use App\Repository\TeamRepository;
+use App\Model\TeamDto;
+use App\Entity\Team;
+use App\Mapper\TeamMapper;
+use App\Model\ResponseDto\TeamResponseDto;
+use App\Service\PaginatorService;
 
 class TeamService
 {
+    public function __construct(
+        private readonly TeamRepository $teamRepository,
+        private readonly TeamMapper $teamMapper,
+        private readonly PaginatorService $paginatorService
+    )
+    {
+    }
 
+    public function create(TeamDto $teamDto): TeamResponseDto
+    {
+        $playersFullName = [];
+        foreach ($teamDto->players as $player){
+            $playerFullName = $player['first_name'].' '.$player['last_name'];
+            if (in_array($playerFullName, $playersFullName)){
+                return new TeamResponseDto(false, "player name ${playerFullName} already exists in team data");
+            }
+            $playersFullName[] = $playerFullName;
+        }
+
+        $team = $this->teamMapper->mapDtoToEntity($teamDto);
+        $teamNameExists = $this->teamRepository->findOneBy(['name' => $team->getName()]);
+        if ($teamNameExists){
+            return new TeamResponseDto(false, 'team name already exists, try another name');
+        }
+
+        $this->teamRepository->save($team, true);
+        return new TeamResponseDto(true, 'team created', $team);
+    }
+
+    public function getAll(int $page, int $limit): PaginatorService
+    {
+        $query = $this->teamRepository->fetchAllTeamsQuery();
+//      If I am able to finish on time, I will implement a cache layer for faster loading,
+//      assuming our system has large dataset and heavy traffic.
+        return $this->paginatorService->paginate($query, $page, $limit);
+    }
 }
