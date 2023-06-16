0f8d0f1d90dd91a00aa0d471da69749c735732bf
Ekoh Franklin <ekohfranklin@gmail.com>
2023-06-16 06:34:00 +0100

    added parent request class for validating incoming requests

--- src/Requests/BaseRequest.php
+++ src/Requests/BaseRequest.php
@@ -2,7 +2,62 @@
 
 namespace App\Requests;
 
-class BaseRequest
-{
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\HttpFoundation\Response;
 
+abstract class BaseRequest
+{
+    public function __construct(protected ValidatorInterface $validator)
+    {
+        $this->populate();
+
+        if ($this->autoValidateRequest()) {
+            $this->validate();
+        }
+    }
+
+    public function validate()
+    {
+        $errors = $this->validator->validate($this);
+
+        $messages = ['status' => 'error', 'message' => 'validation failed', 'errors' => []];
+
+        /** @var \Symfony\Component\Validator\ConstraintViolation  */
+        foreach ($errors as $message) {
+            $messages['errors'][] = [
+                'property' => $message->getPropertyPath(),
+                'value' => $message->getInvalidValue(),
+                'message' => $message->getMessage(),
+            ];
+        }
+
+        if (count($messages['errors']) > 0) {
+            $response = new JsonResponse($messages, Response::HTTP_UNPROCESSABLE_ENTITY);
+            $response->send();
+            exit;
+        }
+    }
+
+    public function getRequest(): Request
+    {
+        return Request::createFromGlobals();
+    }
+
+    protected function populate(): void
+    {
+        foreach ($this->getRequest()->toArray() as $property => $value) {
+            if (property_exists($this, $property)) {
+                $this->{$property} = $value;
+            }
+        }
+    }
+
+    protected function autoValidateRequest(): bool
+    {
+        return true;
+    }
+
+    // Auth config can go here too
 }
