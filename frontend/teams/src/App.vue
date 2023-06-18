ed799df6aff19337978bdd1ad4f4503ba7476fde
Ekoh Franklin <ekohfranklin@gmail.com>
2023-06-18 03:42:22 +0100

    header, table and pagination design

--- frontend/teams/src/App.vue
+++ frontend/teams/src/App.vue
@@ -1,16 +1,67 @@
-<script setup>
+<script>
+  import { defineComponent } from 'vue';
+
+    export default defineComponent({
+      name: 'App',
+      data: () => ({
+        currentPage: 1,
+      }),
+      watch: {
+
+      }
+    })
 </script>
 
 <template>
-  <header>
-
-    <div class="text-3xl font-bold underline open-sans">
-      Hello
-      <!-- <HelloWorld msg="You did it!" /> -->
+  <div id="body-wrap">
+    <!-- Header -->
+    <div class="main-nav">
+      <div class="header">
+        <a class="logostyle">Teams App</a>
+      </div>
     </div>
-  </header>
+    <!-- List teams -->
+    <div class="team-lists mt-4 p-8">
+        <div class="flex">
+          <h3 class="text-3xl font-bold">Teams</h3>
+          <!-- Add plus icon -->
+        </div>
+        <div class="mt-4">
+          <table>
+            <tr>
+              <th>Name</th>
+              <th>Country</th>
+              <th>Money Balance</th>
+              <th>View Players</th>
+            </tr>
+            <tr>
+              <td>Real Madrid</td>
+              <td>Italy</td>
+              <td>500,000,000 USD</td>
+              <td><a>view</a></td>
+            </tr>
+            <tr>
+              <td>Real Madrid</td>
+              <td>Italy</td>
+              <td>500,000,000 USD</td>
+              <td><a>view</a></td>
+            </tr>
+          </table>
+        </div>
+        <!-- pagination -->
+        <div class="mt-4">
+          <vue-awesome-paginate
+            :total-items="50"
+            v-model="currentPage"
+            :items-per-page="5"
+            :max-pages-shown="5"
+            :on-click="onClickHandler"
+          />
+        </div>
+    </div>
+  </div>
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+  
 </style>
--- frontend/teams/src/main.js
+++ frontend/teams/src/main.js
@@ -3,6 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import VueAwesomePaginate from "vue-awesome-paginate"
+
+import "vue-awesome-paginate/dist/style.css"
+
 import './style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueAwesomePaginate)
+app.mount('#app')
