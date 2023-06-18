8c998fc76d9a7c464f8b26dac902e89780b6947d
Ekoh Franklin <ekohfranklin@gmail.com>
2023-06-18 01:10:56 +0100

    added font

--- frontend/teams/src/App.vue
+++ frontend/teams/src/App.vue
@@ -4,38 +4,13 @@
 <template>
   <header>
 
-    <div class="text-3xl font-bold underline">
+    <div class="text-3xl font-bold underline open-sans">
       Hello
       <!-- <HelloWorld msg="You did it!" /> -->
     </div>
   </header>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
 </style>
